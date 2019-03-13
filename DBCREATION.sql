8091299db93ab10cbe077cb8e602b4f65cc4c649
Max Q <maxqfz@gmail.com>
2019-03-14 00:09:49 +0300

    DB creation script added

---
+++ DBCREATION.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE customerservice
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    CONNECTION LIMIT = -1;
+
+CREATE DATABASE accountservice
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    CONNECTION LIMIT = -1;
